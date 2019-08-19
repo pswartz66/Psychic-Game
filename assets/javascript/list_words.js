ef6d7c20435a8b115eedc6efc8fbcaeeb38b127e
Phil Swartz <pswartz.sju@gmail.com>
2019-08-18 22:40:42 -0400

    added a new list of words for the game and added into game.js

--- assets/javascript/list_words.js
+++ assets/javascript/list_words.js
@@ -1,5 +1,5 @@
 
-// Create an array of words related to basketball theme
+// An array of words related to basketball theme
 // All NBA teams city, name, player name
 var words = [
     'Atlanta Hawks',
@@ -133,47 +133,4 @@ var words = [
     'Brook Lopez',	
     'Lonzo Ball']
 
-
-
-// List of all letters in an array
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 
-                'g', 'h', 'i', 'j', 'k', 'l', 
-                'm', 'n', 'o', 'p', 'q', 'r', 
-                's', 't', 'u', 'v', 'w', 'x', 
-                'y', 'z']
-                
-// # of incorrect guesses upon which the game will end
-var incorrect_guesses = 6;
-
-// Total # of guesses = the # of letters in alphabet
-var total_guesses = alphabet.length;
-
-// Each time user guesses a letter store it in
-// user_guessed_letters array
-// reset at game end
-var user_guessed_letters = [];
-
-
-// Variables that hold references to the html id's
-/* var winsText = document.getElementById("wins-text");
-var currentWord = document.getElementById("current-word");
-var placeholderKeys = document.getElementById("placeholder-keys");
-var guessesRemaining = document.getElementById("guesses-remaining");
-var guessedLetters = documment.getElementById("guessed-letters");
- */
-
-
-var computerWord = words[Math.floor(Math.random() * words.length)];
-
-console.log(computerWord);
-
-document.onkeyup = function(event) {
-
-    var userGuess = event.key;
-    
-
-
-
-
-}
 
