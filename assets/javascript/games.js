3e73dd3a38df5ad2f3aefaa8768ff1654581afe1
Phil Swartz <pswartz.sju@gmail.com>
2019-08-18 21:10:36 -0400

    modified index.html and style.css with more p tags to be used for hangman game

---
+++ assets/javascript/games.js
@@ -0,0 +1,18 @@
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 
+                'g', 'h', 'i', 'j', 'k', 'l', 
+                'm', 'n', 'o', 'p', 'q', 'r', 
+                's', 't', 'u', 'v', 'w', 'x', 
+                'y', 'z']
+                
+// # of incorrect guesses upon which the game will end
+var incorrect_guesses = 6;
+
+// total # of guesses = the # of letters in alphabet
+var total_guesses = alphabet.length;
+
+// each time user guesses a letter store it in
+// user_guessed_letters array
+// reset at game end
+var user_guessed_letters = [];
+
+
